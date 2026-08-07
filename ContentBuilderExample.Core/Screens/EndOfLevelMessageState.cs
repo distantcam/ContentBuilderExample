a4a7d510a5b381d0494bdedab3005dd23b0edb59
Cam MacFarland <distantcam@gmail.com>
2026-08-08 07:56:42 +0800

    StarterKit

---
+++ ContentBuilderExample.Core/Screens/EndOfLevelMessageState.cs
@@ -0,0 +1,11 @@
+﻿namespace ContentBuilderExample.Screens;
+
+/// <summary>
+/// The various stage of the pop-up message after completing a level
+/// </summary>
+internal enum EndOfLevelMessageState
+{
+    NotShowing,
+    Show,
+    Showing,
+}
